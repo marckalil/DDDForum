a142c4ffff5b8e1155f312da50583f1c1254a0e3
Marc Kalil <marc@marckalil.com>
2024-11-15 23:08:36 +0100

    set up and query database with prisma on localhost

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const app = express();
 app.use(express.json()); // To parse JSON bodies
 /*
@@ -16,17 +18,59 @@ app.use(cors());
 
 // Create user
 app.post("/users/new", async (req: Request, res: Response ) => {
-	res.send("Create user");
+	try {
+	const { email, username, firstName, lastName, password } = req.body;
+	const user = await prisma.user.create({
+		data: {
+			email,
+			username,
+			firstName,
+			lastName,
+			password
+		}
+		
+	});
+	res.status(201).json(user.id);
+	} catch (error) {
+		res.status(400).json({ error: "Something went wrong" });
+	}
 });
 
 // Edit user
 app.post("/users/edit/:userId", async (req: Request, res: Response ) => {
-	res.send("Edit user");
+	try {
+		const { userId } = req.params;
+		const { email, username, firstName, lastName } = req.body;
+		const user = await prisma.user.update({
+			where: {
+				id: parseInt(userId)
+			},
+			data: {
+				email,
+				username,
+				firstName,
+				lastName
+			}
+		});
+		res.status(200).json(user);
+	} catch (error) {
+		res.status(400).json({ error: "Something went wrong" });
+	}	
 });
 
 // Get a user by e-mail
 app.get("/users", async (req: Request, res: Response ) => {
-	res.send("Get user by e-mail");
+	try {
+		const email = req.query.email as string;
+		const user = await prisma.user.findUnique({
+			where: {
+				email
+			}
+		});
+		res.status(200).json(user);
+	} catch (error) {
+		res.status(400).json({ error: "Something went wrong" });
+	}
 });
 
 const port = process.env.PORT ?? 3000;
