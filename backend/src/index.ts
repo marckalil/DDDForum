850970f5f2ae0a04e0bc1d3a46db63977700735f
Marc Kalil <marc@marckalil.com>
2024-11-15 12:38:50 +0100

    Setup basic express server

---
+++ backend/src/index.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json()); // To parse JSON bodies
+/*
+	* Allow requests from any origin
+	* This is useful when developing locally
+	* In production, you should restrict the origin to your frontend's URL
+	* For example: app.use(cors({ origin: "https://your-frontend-url.com" }));
+	* This will block requests from any other origin
+	* This is a security measure to prevent Cross-Site Request Forgery (CSRF) attacks
+	* Learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+*/
+app.use(cors());
+
+// Create user
+app.post("/users/new", async (req: Request, res: Response ) => {
+	res.send("Create user");
+});
+
+// Edit user
+app.put("/users/edit/:userId", async (req: Request, res: Response ) => {
+	res.send("Edit user");
+});
+
+// Get a user by e-mail
+app.get("/users", async (req: Request, res: Response ) => {
+	res.send("Get user by e-mail");
+});
+
+const port = process.env.PORT ?? 3000;
+
+app.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`);
+});
