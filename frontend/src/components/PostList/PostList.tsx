d72e9bef60c30102bdae35dd66725df160f82b4f
Marc Kalil <marc@marckalil.com>
2024-11-29 21:39:41 +0100

    refacto post components

--- frontend/src/components/PostList/PostList.tsx
+++ frontend/src/components/PostList/PostList.tsx
@@ -2,31 +2,14 @@ import { FlatList, StyleSheet } from 'react-native';
 
 import { PostItem } from '../PostItem';
 
-import type { Post, Vote } from '@/src/types';
-
-function getPostPoints(votes: Vote[]): number {
-  let points = 0;
-  for (const vote of votes) {
-    points += vote.voteType === 'Upvote' ? 1 : -1;
-  }
-  return points;
-}
-
-const sortPostsByPopularity = (
-  posts: Post[]
-): { post: Post; popularity: number }[] => {
-  return posts
-    .map((post) => ({ post, popularity: getPostPoints(post.votes) }))
-    .sort((a, b) => b.popularity - a.popularity);
-};
+import type { Post } from '@/src/types';
 
 export function PostList({ posts }: { posts: Post[] }) {
-  const postsByPopularity = sortPostsByPopularity(posts);
   return (
     <FlatList
-      data={postsByPopularity}
-      renderItem={({ item }) => <PostItem {...item} />}
-      keyExtractor={(item) => item.post.id.toString()}
+      data={posts}
+      renderItem={({ item }) => <PostItem post={item} />}
+      keyExtractor={(item) => item.id.toString()}
       contentContainerStyle={styles.contentContainer}
     />
   );
